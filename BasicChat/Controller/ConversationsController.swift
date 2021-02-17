a75ea326c85f88853c247478468c755490e84cf0
agileassociates <agileassociates@agileastessMini.lan>
2021-02-16 21:05:48 -0500

    setup home screen

---
+++ BasicChat/Controller/ConversationsController.swift
@@ -0,0 +1,39 @@
+//
+//  ConversationsController.swift
+//  BasicChat
+//
+//  Created by agileassociates on 2/16/21.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showProfile() {
+        print(123)
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+
+    }
+}
