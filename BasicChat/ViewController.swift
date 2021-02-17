31b9c5e460c0183170598b0ce2e7ab2fc3552bfa
agileassociates <agileassociates@agileastessMini.lan>
2021-02-16 20:36:05 -0500

    first commit

---
+++ BasicChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicChat
+//
+//  Created by agileassociates on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
