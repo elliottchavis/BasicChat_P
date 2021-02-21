338b444b6cb5a34458a7074e163a850ce09b3621
agileassociates <agileassociates@agileastessMini.lan>
2021-02-20 19:20:34 -0500

    fetch user data

---
+++ BasicChat/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  BasicChat
+//
+//  Created by agileassociates on 2/20/21.
+//
+
+import UIKit
+
+struct User {
+    let uid: String
+    let profileImageUrl: String
+    let username: String
+    let fullname: String
+    let email: String
+}
