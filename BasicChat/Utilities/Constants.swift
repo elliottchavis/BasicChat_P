5178c36c186736a7fb7b66b34f7081808034b896
agileassociates <agileassociates@agileastessMini.lan>
2021-02-23 23:32:18 -0500

    upload message to database

---
+++ BasicChat/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  BasicChat
+//
+//  Created by agileassociates on 2/23/21.
+//
+
+import Firebase
+
+let COLLECTION_MESSAGES = Firestore.firestore().collection("messages")
